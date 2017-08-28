76b07dffbdf43300d7c79bb071eb2b304a1fa81a
ctrl-freak <bryce.sheehan@gmail.com>
2017-08-28 10:21:38 +0800

    File/folder creation conflict with %~nx1, changed to %~n1
    When creating folder, use (apk) file name without extension

--- extract.bat
+++ extract.bat
@@ -3,9 +3,9 @@
 if [%1]==[] goto usage
 
 cd %~dp0
-mkdir %~nx1
-7z e -ir!META-INF\*.RSA %1 -o%~nx1\ -aoa
-for /f %%f in ('dir /b %~nx1\') do openssl pkcs7 -inform DER -in %~nx1\%%f -noout -print_certs -text | MORE /P > %~nx1\%%f.txt
+mkdir %~n1\
+7z e -ir!META-INF\*.RSA %1 -o%~n1\ -aoa
+for /f %%f in ('dir /b %~n1\') do openssl pkcs7 -inform DER -in %~n1\%%f -noout -print_certs -text | MORE /P > %~n1\%%f.txt
 goto :eof
 
 :usage
