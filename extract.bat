7f819e052f4b9b6aa5bf780815e8012a75899248
ctrl-freak <bryce.sheehan@gmail.com>
2018-09-12 14:16:35 +0800

    Output certs in same directory as APK, not batch

--- extract.bat
+++ extract.bat
@@ -3,9 +3,9 @@
 if [%1]==[] goto usage
 
 cd %~dp0
-mkdir %~n1\
-7z e -ir!META-INF\*.RSA %1 -o%~n1\ -aoa
-for /f %%f in ('dir /b %~n1\') do openssl pkcs7 -inform DER -in %~n1\%%f -noout -print_certs -text | MORE /P > %~n1\%%f.txt
+mkdir %~dpn1\
+7z e -ir!META-INF\*.RSA %1 -o%~dpn1\ -aoa
+for /f %%f in ('dir /b %~dpn1\') do openssl pkcs7 -inform DER -in %~dpn1\%%f -noout -print_certs -text | MORE /P > %~dpn1\%%f.txt
 goto :eof
 
 :usage
